1feaa9d65c4a418731c4235a1f0e079fd9c52edd
Ankit Sharma <findme.ankit@gmail.com>
2019-02-25 17:36:50 +0000

    Adding modules for vpc app and static in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,21 @@
+#Using provider as AWS
 provider "aws" {
    access_key = "${var.aws_access_key}" 
    secret_key = "${var.aws_secret_key}"
    region     = "${var.aws_region}"
 }
+
+#VPC module
+module "vpc" {
+   source = "./vpc"
+}
+
+module "static_web" {
+   source = "./static"
+}
+
+module "application" {
+   source = "./app"
+}
+
+
