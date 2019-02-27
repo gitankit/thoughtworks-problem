dc4be5249e8416256e7b1d6055f261e370966084
Ankit Sharma <findme.ankit@gmail.com>
2019-02-27 19:17:16 +0000

    Adding pub priv keys common variables and app sgs

---
+++ terraform/app/sg.tf
@@ -0,0 +1,14 @@
+resource "aws_security_group" "app" {
+   name = "application"
+   description = "Used for tomcat web application"
+   vpc_id = "${var.vpc_id}"
+   
+   ingress {
+      from_port = 80
+      to_port   = 80
+      protocol  = "http"
+      security_groups = []
+   }
+
+  
+}
