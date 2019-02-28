c40e11acb999bf9b8b756e6fe0f7602c2e92aab0
Ankit Sharma <findme.ankit@gmail.com>
2019-02-28 18:03:59 +0000

    Forgot to add static sg

---
+++ terraform/static/sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "static" {
+   name = "static-resources"
+   description = "Used for static images and styles"
+   vpc_id = "${var.vpc_id}"
+   
+   ingress {
+      from_port = 80
+      to_port   = 80
+      protocol  = "http"
+      security_groups = []
+   }
+
+  
+}
+
+output "sg_id" {
+   value = "${aws_security_group.static.id}"
+}
